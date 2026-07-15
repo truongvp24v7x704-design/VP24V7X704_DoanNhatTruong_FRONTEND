4f9ad0d5aa077dfe1dae3ea2d0cf9e66695642dc
truongdoan021991-jpg <truongdoan021991@gmail.com>
2026-07-15 22:06:00 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page">
+    <p>
+      Oops, không thể tìm thấy trang.
+      Trở về
+      <router-link to="/">trang chủ.</router-link>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.page {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
